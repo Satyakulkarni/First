812c3022896e845bfc3d560d275a0dec09a445f2
kulkarni <kulkarni@kulkarni-PC>
2017-12-07 15:35:48 +0530

    Initial checking

---
+++ First/src/First/First.java
@@ -0,0 +1,25 @@
+package First;
+
+public class First {
+	String name;
+	int rollno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}   
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	 
+	public void display()
+	{
+		
+		System.out.println("Display");
+	}
+
+}
---
+++ First/src/First/Second.java
@@ -0,0 +1,12 @@
+package First;
+
+public class Second {
+	public static void main(String[] args)
+	{
+		Second s=new Second();
+		First f= new First();
+		f.display();
+		
+		}
+
+}
